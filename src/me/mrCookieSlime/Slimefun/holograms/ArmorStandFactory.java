b1c3bc1baaabe6fadef2d97c451661b86fd705f8
lordmampf <lordmampf@users.noreply.github.com>
2016-09-21 21:33:20 +0200

    maybe fix bug with infused hopper and 17k armorstands

--- src/me/mrCookieSlime/Slimefun/holograms/ArmorStandFactory.java
+++ src/me/mrCookieSlime/Slimefun/holograms/ArmorStandFactory.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
 
 import me.mrCookieSlime.CSCoreLibPlugin.general.Reflection.ReflectionUtils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
@@ -11,7 +12,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.EulerAngle;
 
 public class ArmorStandFactory {
-	
+
 	public static ArmorStand createHidden(Location l) {
 		ArmorStand armorstand = (ArmorStand) l.getWorld().spawnEntity(l, EntityType.ARMOR_STAND);
 		armorstand.setVisible(false);
@@ -21,41 +22,39 @@ public class ArmorStandFactory {
 		armorstand.setRemoveWhenFarAway(false);
 		try {
 			Object nmsEntity = armorstand.getClass().getMethod("getHandle").invoke(armorstand);
-            if (ReflectionUtils.getVersion().startsWith("v1_9_")) {
-            	try {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bz", 2039583);
-            	} catch(IllegalArgumentException x) {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
-            	}
-            }
-            else if (ReflectionUtils.getVersion().startsWith("v1_10_")) {
-            	try {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bB", 2039583);
-            	} catch(IllegalArgumentException x) {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
-            	}
-            }
-            else {
-            	Method method = nmsEntity.getClass().getMethod("getNBTTag");
-            	Object tag = method.invoke(nmsEntity);
-                if(tag == null) {
-                    tag = ReflectionUtils.getClass("NBTTagCompound").newInstance();
-                }
-                method = nmsEntity.getClass().getMethod("c", ReflectionUtils.getClass("NBTTagCompound"));
-                method.invoke(nmsEntity, tag);
-    			
-                tag.getClass().getMethod("setBoolean", String.class, boolean.class).invoke(tag, "Invulnerable", true);
-                nmsEntity.getClass().getMethod("f", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
-                
-                tag.getClass().getMethod("setInt", String.class, int.class).invoke(tag, "DisabledSlots", 2039583);
-                nmsEntity.getClass().getMethod("a", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
-            }
-        } catch (Exception x) {
-            x.printStackTrace();
-        }
+			if (ReflectionUtils.getVersion().startsWith("v1_9_")) {
+				try {
+					ReflectionUtils.setFieldValue(nmsEntity, "bz", 2039583);
+				} catch (IllegalArgumentException x) {
+					ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
+				}
+			} else if (ReflectionUtils.getVersion().startsWith("v1_10_")) {
+				try {
+					ReflectionUtils.setFieldValue(nmsEntity, "bB", 2039583);
+				} catch (IllegalArgumentException x) {
+					ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
+				}
+			} else {
+				Method method = nmsEntity.getClass().getMethod("getNBTTag");
+				Object tag = method.invoke(nmsEntity);
+				if (tag == null) {
+					tag = ReflectionUtils.getClass("NBTTagCompound").newInstance();
+				}
+				method = nmsEntity.getClass().getMethod("c", ReflectionUtils.getClass("NBTTagCompound"));
+				method.invoke(nmsEntity, tag);
+
+				tag.getClass().getMethod("setBoolean", String.class, boolean.class).invoke(tag, "Invulnerable", true);
+				nmsEntity.getClass().getMethod("f", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
+
+				tag.getClass().getMethod("setInt", String.class, int.class).invoke(tag, "DisabledSlots", 2039583);
+				nmsEntity.getClass().getMethod("a", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
+			}
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
 		return armorstand;
 	}
-	
+
 	public static ArmorStand createSmall(Location l, ItemStack item, EulerAngle arm, float yaw) {
 		l.setYaw(yaw);
 		ArmorStand armorstand = (ArmorStand) l.getWorld().spawnEntity(l, EntityType.ARMOR_STAND);
@@ -69,34 +68,33 @@ public class ArmorStandFactory {
 		armorstand.setRemoveWhenFarAway(false);
 		try {
 			Object nmsEntity = armorstand.getClass().getMethod("getHandle").invoke(armorstand);
-            if (ReflectionUtils.getVersion().startsWith("v1_9_")) {
-            	try {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bz", 2039583);
-            	} catch(IllegalArgumentException x) {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
-            	}
-            }
-            else {
-            	Method method = nmsEntity.getClass().getMethod("getNBTTag");
-            	Object tag = method.invoke(nmsEntity);
-                if(tag == null) {
-                    tag = ReflectionUtils.getClass("NBTTagCompound").newInstance();
-                }
-                method = nmsEntity.getClass().getMethod("c", ReflectionUtils.getClass("NBTTagCompound"));
-                method.invoke(nmsEntity, tag);
-    			
-                tag.getClass().getMethod("setBoolean", String.class, boolean.class).invoke(tag, "Invulnerable", true);
-                nmsEntity.getClass().getMethod("f", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
-                
-                tag.getClass().getMethod("setInt", String.class, int.class).invoke(tag, "DisabledSlots", 2039583);
-                nmsEntity.getClass().getMethod("a", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
-            }
-        } catch (Exception x) {
-            x.printStackTrace();
-        }
+			if (ReflectionUtils.getVersion().startsWith("v1_9_")) {
+				try {
+					ReflectionUtils.setFieldValue(nmsEntity, "bz", 2039583);
+				} catch (IllegalArgumentException x) {
+					ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
+				}
+			} else {
+				Method method = nmsEntity.getClass().getMethod("getNBTTag");
+				Object tag = method.invoke(nmsEntity);
+				if (tag == null) {
+					tag = ReflectionUtils.getClass("NBTTagCompound").newInstance();
+				}
+				method = nmsEntity.getClass().getMethod("c", ReflectionUtils.getClass("NBTTagCompound"));
+				method.invoke(nmsEntity, tag);
+
+				tag.getClass().getMethod("setBoolean", String.class, boolean.class).invoke(tag, "Invulnerable", true);
+				nmsEntity.getClass().getMethod("f", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
+
+				tag.getClass().getMethod("setInt", String.class, int.class).invoke(tag, "DisabledSlots", 2039583);
+				nmsEntity.getClass().getMethod("a", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
+			}
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
 		return armorstand;
 	}
-	
+
 	public static ArmorStand createSmall(Location l, ItemStack head, float yaw) {
 		l.setYaw(yaw);
 		ArmorStand armorstand = (ArmorStand) l.getWorld().spawnEntity(l, EntityType.ARMOR_STAND);
@@ -108,31 +106,30 @@ public class ArmorStandFactory {
 		armorstand.setRemoveWhenFarAway(false);
 		try {
 			Object nmsEntity = armorstand.getClass().getMethod("getHandle").invoke(armorstand);
-            if (ReflectionUtils.getVersion().startsWith("v1_9_")) {
-            	try {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bz", 2039583);
-            	} catch(IllegalArgumentException x) {
-            		ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
-            	}
-            }
-            else {
-            	Method method = nmsEntity.getClass().getMethod("getNBTTag");
-            	Object tag = method.invoke(nmsEntity);
-                if(tag == null) {
-                    tag = ReflectionUtils.getClass("NBTTagCompound").newInstance();
-                }
-                method = nmsEntity.getClass().getMethod("c", ReflectionUtils.getClass("NBTTagCompound"));
-                method.invoke(nmsEntity, tag);
-    			
-                tag.getClass().getMethod("setBoolean", String.class, boolean.class).invoke(tag, "Invulnerable", true);
-                nmsEntity.getClass().getMethod("f", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
-                
-                tag.getClass().getMethod("setInt", String.class, int.class).invoke(tag, "DisabledSlots", 2039583);
-                nmsEntity.getClass().getMethod("a", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
-            }
-        } catch (Exception x) {
-            x.printStackTrace();
-        }
+			if (ReflectionUtils.getVersion().startsWith("v1_9_")) {
+				try {
+					ReflectionUtils.setFieldValue(nmsEntity, "bz", 2039583);
+				} catch (IllegalArgumentException x) {
+					ReflectionUtils.setFieldValue(nmsEntity, "bA", 2039583);
+				}
+			} else {
+				Method method = nmsEntity.getClass().getMethod("getNBTTag");
+				Object tag = method.invoke(nmsEntity);
+				if (tag == null) {
+					tag = ReflectionUtils.getClass("NBTTagCompound").newInstance();
+				}
+				method = nmsEntity.getClass().getMethod("c", ReflectionUtils.getClass("NBTTagCompound"));
+				method.invoke(nmsEntity, tag);
+
+				tag.getClass().getMethod("setBoolean", String.class, boolean.class).invoke(tag, "Invulnerable", true);
+				nmsEntity.getClass().getMethod("f", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
+
+				tag.getClass().getMethod("setInt", String.class, int.class).invoke(tag, "DisabledSlots", 2039583);
+				nmsEntity.getClass().getMethod("a", ReflectionUtils.getClass("NBTTagCompound")).invoke(nmsEntity, tag);
+			}
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
 		return armorstand;
 	}
 
--- src/me/mrCookieSlime/Slimefun/holograms/InfusedHopper.java
+++ src/me/mrCookieSlime/Slimefun/holograms/InfusedHopper.java
@@ -6,18 +6,19 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 
 public class InfusedHopper {
-	
+
 	private static final double offset = 1.2;
-	
+
 	public static ArmorStand getArmorStand(Block hopper) {
 		Location l = new Location(hopper.getWorld(), hopper.getX() + 0.5, hopper.getY() + offset, hopper.getZ() + 0.5);
-		
-		for (Entity n: l.getChunk().getEntities()) {
+
+		for (Entity n : l.getChunk().getEntities()) {
 			if (n instanceof ArmorStand) {
-				if (n.getCustomName() == null && l.distanceSquared(n.getLocation()) < 0.4D) return (ArmorStand) n;
+				if (n.getCustomName() == null && l.distance(n.getLocation()) < 1D)
+					return (ArmorStand) n;
 			}
 		}
-		
+
 		ArmorStand hologram = ArmorStandFactory.createHidden(l);
 		hologram.setCustomNameVisible(false);
 		hologram.setCustomName(null);
